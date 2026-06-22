35b2a5776e998bd12233845df39c38464bea89b7
xuming <920972751@qq.com>
2026-06-23 07:51:31 +0800

    实现 dwpose

--- nets/audio/base.py
+++ nets/audio/base.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# @FileName      : base.py
+# @Time          : 2026-06-22 18:18:12
+# @Author        : XuMing
+# @Email         : 920972751@qq.com
+# @description   : TODO
+# @Company       : 2026 XuMing. All Rights Reserved.
+"""
+
+
+
+
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Any
